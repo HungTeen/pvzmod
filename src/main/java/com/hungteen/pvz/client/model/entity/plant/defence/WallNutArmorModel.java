4b153641cac1f2702fd715ba31d1caf2d99af570
GrassCarp <735629912@qq.com>
2022-03-02 15:58:54 +0800

    to ht

--- src/main/java/com/hungteen/pvz/client/model/entity/plant/defence/WallNutArmorModel.java
+++ src/main/java/com/hungteen/pvz/client/model/entity/plant/defence/WallNutArmorModel.java
@@ -2,6 +2,8 @@ package com.hungteen.pvz.client.model.entity.plant.defence;
 
 import com.hungteen.pvz.client.model.entity.plant.PVZPlantModel;
 import com.hungteen.pvz.common.entity.plant.defence.WallNutEntity;
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.client.renderer.entity.model.EntityModel;
 import net.minecraft.client.renderer.model.ModelRenderer;
 
@@ -36,5 +38,15 @@ public class WallNutArmorModel<T extends WallNutEntity> extends PVZPlantModel<T>
 	@Override
 	public EntityModel<T> getPlantModel() {
 		return this;
+	}
+
+
+	@Override
+	public void renderToBuffer(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
+		body.render(matrixStack, buffer, packedLight, packedOverlay);
+	}
+
+	public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay){
+		body.render(matrixStack, buffer, packedLight, packedOverlay);
 	}
 }
